da1cf90bca7336fd38ea82c6dda8da511aea1b89
Luigi Mendoza <luigi@dgtalliance.com>
2019-12-10 18:00:06 -0500

    Add prop-types dependency

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from './../constants/weathers';
 
 const WeatherData = () => (
   <div>
-    <WeatherTemperature temperature={20} weatherState={''}></WeatherTemperature>
-    <WeatherExtraInfo humidity={80} wind={'10m/s'}></WeatherExtraInfo>
+    <WeatherTemperature 
+      temperature={20} 
+      weatherState={SUN}
+    />
+    <WeatherExtraInfo humidity={80} wind={'10m/s'} />
   </div>
 );
 
